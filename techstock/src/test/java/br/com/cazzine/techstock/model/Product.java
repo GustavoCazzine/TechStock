ad571d45ea60403bfa0c29f5e7f3238ede577dd2
Gustavo Cazzine <cazzinengustavo@gmail.com>
2026-02-19 19:04:47 -0300

    feat: implementa classe base para registro dos produtos

--- techstock/src/test/java/br/com/cazzine/techstock/model/Product.java
+++ techstock/src/test/java/br/com/cazzine/techstock/model/Product.java
@@ -1,4 +1,50 @@
 package br.com.cazzine.techstock.model;
 
-public class Product {
+public abstract class  Product {
+    private Integer id;
+    private String name;
+    private double price;
+    private int amount;
+
+    public Product() {
+    }
+
+    public Product(Integer id, String name, double price, int amount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
